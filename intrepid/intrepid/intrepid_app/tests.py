17b0393c7a488c80b5e0afde243b43c29d7200f4
DileepKumarMantena <deelipkumar261997@gmail.com>
2024-06-15 00:33:35 +0530

    Added new styles and updated the code

--- intrepid/intrepid/intrepid_app/tests.py
+++ intrepid/intrepid/intrepid_app/tests.py
@@ -1,8 +1,6 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from .models import DashboardItem, Booking, Package
-from .forms import PackageForm, NewPackageForm
-from django.contrib.auth.models import User
 
 class DashboardItemsViewTest(TestCase):
     def setUp(self):
